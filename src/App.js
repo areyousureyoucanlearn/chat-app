763ae2e0e21d62592a556f3b819949665a76bb73
Nikolas Nesterenko <steepdog3@gmail.com>
2022-05-07 22:30:02 +0300

    add: messages

--- src/App.js
+++ src/App.js
@@ -20,7 +20,27 @@ function App() {
         </button>
       </header>
       <div className='messages-frame'>
-        
+        <div className='date'>
+          <span>2/2/2222</span>
+        </div>
+        <div className='selfMessage'>
+          <span>Hi cutie!</span>
+          <div className='timeMessage'>
+              time
+          </div>
+        </div>
+        <div className='otherUser'>
+          <div className='iconUser'>
+            icon
+          </div>
+          <div className='otherMessage'>
+            <div><span>Name</span></div>
+            <span>Hi poop!</span>
+            <div className='timeMessage'>
+              time
+            </div>
+          </div>
+        </div>
       </div>
       <footer className='footer'>
         <button>
