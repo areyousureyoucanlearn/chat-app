1f9d2690201ac750d529310e284d0bf64d632747
Nikolas Nesterenko <steepdog3@gmail.com>
2022-05-08 19:24:01 +0300

    fix: improved code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Header from './Header';
 import Messages from './Messages';
 import SendMessage from './SendMessage';
+import SelfMessage from './SelfMessage';
+import OtherMessage from './OtherMessage';
 import './App.css';
 
 
@@ -8,7 +10,10 @@ function App() {
   return (
     <div className="App">
       <Header title="🦄 Team Unicorns" description = "last seen 45 minutes ago"/>
-      <Messages date="08/05/2022" />
+      <Messages date="08/05/2022">
+        <SelfMessage message="Hi cuite!" date="19:07"/>
+        <OtherMessage user="Ivan" message="Hi poop!" date="19:08"/>
+      </Messages>
       <SendMessage/>
     </div>
   );
--- src/Messages.js
+++ src/Messages.js
@@ -1,15 +1,12 @@
 import './Messages.css'
-import SelfMessage from './SelfMessage';
-import OtherMessage from './OtherMessage';
 
-function Messages({date}){
+function Messages({date, children}){
     return (
         <div className='messages-frame'>
             <div className='date'>
                 <span>{date}</span>
             </div>
-            <SelfMessage message="Hi cuite!" date="19:07"/>
-            <OtherMessage user="Ivan" message="Hi poop!" date="19:08"/>
+            {children}
         </div>
     )
 }
