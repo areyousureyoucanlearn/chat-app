8ba6938b06ca235e2a92d97bcbc72111df01aaf0
Nikolas Nesterenko <steepdog3@gmail.com>
2022-05-08 19:52:04 +0300

    add: new icons

--- src/SendMessage.js
+++ src/SendMessage.js
@@ -1,23 +1,20 @@
 import './SendMessage.css'
+import { PaperAirplaneIcon } from '@heroicons/react/solid'
+import { AtSymbolIcon } from '@heroicons/react/solid'
+import { EmojiHappyIcon } from '@heroicons/react/solid'
 
 function SendMessage() {
     return (
         <footer className='footer'>
-        <button>
-          <img alt='smile' src="icon-smile.png"/>
-        </button>
-        <form className='sendMessage'>
-          <input placeholder='message' />
-        </form>
-        <div className='buttonFormR'>
-          <button>
-            <img alt='mention' src="icon-mention.png" />
-          </button>
-          <button>
-            <img alt='send' src="send-message.png"/>
-          </button>
-        </div>
-      </footer>
+          <EmojiHappyIcon className="icon"/>
+          <form className='sendMessage'>
+            <input placeholder='message' />
+          </form>
+          <div className='buttonFormR'>
+            <AtSymbolIcon className="icon"/>
+            <PaperAirplaneIcon className="icon"/>
+          </div>
+        </footer>
     )
 }
 
