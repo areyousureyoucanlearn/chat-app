aca0028a176ca007c4e7a81252e76214cdd6def4
Ivan Stasiuk <brokeyourbike@gmail.com>
2022-05-09 11:27:06 +0300

    feat: render messages from the list

--- src/App.js
+++ src/App.js
@@ -5,14 +5,26 @@ import SelfMessage from './SelfMessage';
 import OtherMessage from './OtherMessage';
 import './App.css';
 
-
 function App() {
+  const userId = 5;
+
+  const messages = [
+    {id:1, name: "Ivan", text:"Hello!", userId: 3},
+    {id:2, name: "Ivan", text:"Are you here?", userId: 3},
+    {id:3, name: "Andriy", text:"NO!", userId: 5},
+  ];
+
   return (
     <div className="App">
-      <Header title="🦄 Team Unicorns" description = "last seen 45 minutes ago"/>
+      <Header title="🦄 Team Unicorns" description="last seen 45 minutes ago" />
       <Messages date="08/05/2022">
-        <SelfMessage message="Hi cuite!" date="19:07"/>
-        <OtherMessage user="Ivan" message="Hi poop!" date="19:08"/>
+        {messages.map((message) => {
+          if (message.userId === userId) {
+            return <SelfMessage message={message.text} date={message.id} />
+          } else {
+            return <OtherMessage user={message.name} message={message.text} date={message.id} />
+          }
+        })}
       </Messages>
       <SendMessage/>
     </div>
