f0837f518e6cfae7e4ee1e3e3942b3edb99e51af
Nikolas Nesterenko <steepdog3@gmail.com>
2022-05-10 15:45:48 +0300

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -2,11 +2,14 @@ import './Header.css';
 import { DotsHorizontalIcon } from '@heroicons/react/solid'
 
 function Header({title, description, users}) {
+
+  const userGroup = [...users].reverse().splice(0,users.length-(users.length-4))
+  
   return (
       <header className="App-header">
         <div className='Avatar-group'>
         {
-          [...users].reverse().splice(0,users.length-(users.length-4)).map((user, index) => {
+          userGroup.map((user, index) => {
             return (
               <div id={user.id} key={user.id} className='Avatar' style={{left: index*30+'px'}}>
                 <img src={user.image} alt={user.name}></img>
